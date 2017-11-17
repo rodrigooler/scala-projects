6bfc3d4f479a4942b30fe964155c5ac2b14485e1
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-11-16 22:48:28 -0200

    Create String

---
+++ src/main/scala/examples/String.scala
@@ -0,0 +1,10 @@
+package examples;
+
+object String extends App {
+  var firstName : String = "Rodrigo"
+  var lastName : String = "Oler"
+
+  println(firstName)
+  println(lastName)
+  println(firstName + " "+ lastName)
+}
