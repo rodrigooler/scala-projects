ef4e8f961919ab8b45c7e3dcb71eeb92bf371a33
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-11-16 23:33:44 -0200

    Create Byte

---
+++ src/main/scala/examples/Byte.scala
@@ -0,0 +1,10 @@
+package examples
+
+object Byte extends App {
+  var byte : Byte = 2
+  var byte2 : Byte = 10
+  var byte3 : Byte = 100
+//  var byte4: Byte = 200
+
+  println(byte, byte2, byte3)
+}
