0450ed40a30696549ce15276c1bffabc809f677d
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-11-16 23:02:04 -0200

    Create Comment

---
+++ src/main/scala/examples/Comment.scala
@@ -0,0 +1,10 @@
+package examples
+
+object Comment extends App {
+//  var firstName : String = "Rodrigo"
+//  var lastName : String = "Oler"
+//
+//  println(firstName)
+//  println(lastName)
+//  println(firstName + " "+ lastName)
+}
