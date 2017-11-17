216947031cc2d6c1213dfe1aad0c056d773d4e82
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-11-16 23:03:18 -0200

    Create BlockedComment

---
+++ src/main/scala/examples/BlockedComment.scala
@@ -0,0 +1,12 @@
+package examples
+
+object BlockedComment extends App {
+  /*
+  var firstName : String = "Rodrigo"
+  var lastName : String = "Oler"
+
+  println(firstName)
+  println(lastName)
+  println(firstName + " "+ lastName)
+  */
+}
