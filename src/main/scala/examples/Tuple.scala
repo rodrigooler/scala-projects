e1d2fe33e3dda134911d52ac6a38f48cd7a27e32
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-11-17 01:28:34 -0200

    Add inference

--- src/main/scala/examples/Tuple.scala
+++ src/main/scala/examples/Tuple.scala
@@ -2,4 +2,5 @@ package examples
 
 object Tuple extends App {
   val (int: Int, string: String) = Pair(40, "Foo"); println(int, string)
+  val (int2, string2) = Pair(100, "Roo"); println(int2, string2)
 }
