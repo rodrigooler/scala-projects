9717d09c69bae312062ce830f12eb39070f314e1
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-11-17 01:32:30 -0200

    Create PrivateExample

---
+++ src/main/scala/examples/PrivateExample.scala
@@ -0,0 +1,11 @@
+package examples
+
+class PrivateExample {
+  class Inner {
+    private def f() { println("f") }
+    class InnerMost {
+      f() // OK
+    }
+  }
+  (new Inner).f() // Error: f is not accessible
+}
